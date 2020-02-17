ff4bc497be2a17efdeff792cace941a9f0d28495
Salman Bukhari <salmanbukhari37@gmail.com>
2020-02-17 16:42:45 +0500

    first commit

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FCM</title>
+    <link rel="manifest" href="./manifest.json">
     <!-- The core Firebase JS SDK is always required and must be listed first -->
-    <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-app.js"></script>
 
+    <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-messaging.js"></script>
+    
     <!-- TODO: Add SDKs for Firebase products that you want to use
         https://firebase.google.com/docs/web/setup#available-libraries -->
     <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-analytics.js"></script>
+
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 
     <script>
     // Your web app's Firebase configuration
@@ -26,6 +34,72 @@
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
+
+    // Retrieve Firebase Messaging object.
+    const messaging = firebase.messaging(); 
+
+    // Get Instance ID token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    messaging.getToken().then((currentToken) => {
+    if (currentToken) {
+        console.log(currentToken);
+        saveToken(currentToken);
+
+        sendTokenToServer(currentToken);
+        // updateUIForPushEnabled(currentToken);
+    } else {
+        // Show permission request.
+        console.log('No Instance ID token available. Request permission to generate one.');
+        // Show permission UI.
+        // updateUIForPushPermissionRequired();
+        setTokenSentToServer(false);
+    }
+    }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+        showToken('Error retrieving Instance ID token. ', err);
+        setTokenSentToServer(false);
+    });
+
+    function showToken(currentToken) {
+        // Show token in console and UI.
+        const tokenElement = document.querySelector('#token');
+        tokenElement.textContent = currentToken;
+    }
+
+    // Send the Instance ID token your application server, so that it can:
+    // - send messages back to this app
+    // - subscribe/unsubscribe the token from topics
+    function sendTokenToServer(currentToken) {
+        if (!isTokenSentToServer()) {
+        console.log('Sending token to server...');
+        // TODO(developer): Send the current token to your server.
+        setTokenSentToServer(true);
+        } else {
+        console.log('Token already sent to server so won\'t send it again ' +
+            'unless it changes');
+        }
+
+    }
+
+    function isTokenSentToServer() {
+        return window.localStorage.getItem('sentToServer') === '1';
+    }
+
+    function setTokenSentToServer(sent) {
+        window.localStorage.setItem('sentToServer', sent ? '1' : '0');
+    }
+
+    function saveToken( token ) {
+        $.ajax({
+            data: {token: token},
+            type: 'POST',
+            url: 'saveFcmToken.php',
+            success: (res) => {
+                
+            }
+        });
+    }
+
     </script>
 </head>
 <body>
---
+++ saveFcmToken.php
@@ -0,0 +1,21 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fcm";
+
+if ( isset($_POST['token']) ) {
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO tokens (token, created) VALUES ('{$_POST['token']}', '2020-02-14 04:52:00')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "New record created successfully";
+    }
+    catch(PDOException $e)
+    {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+}
